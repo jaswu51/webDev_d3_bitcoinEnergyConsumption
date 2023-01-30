26fbb733d9013d74578351f5cc77ab0e4cee3d82
jaswu51 <506346013@qq.com>
2023-01-30 18:51:38 +0100

    push

--- js/map.js
+++ js/map.js
@@ -1,58 +1,74 @@
 // The svg
-var svg = d3.select("svg"),
+const svg = d3.select("svg"),
   width = +svg.attr("width"),
   height = +svg.attr("height");
 
+ tip=d3.select("svg").append("div")
+  .attr("class", "tooltip")
+  .style("opacity", 0.5);
+
 // Map and projection
-var path = d3.geoPath();
-var projection = d3.geoMercator()
-  .scale(120)
+const path = d3.geoPath();
+const projection = d3.geoMercator()
+  .scale(70)
   .center([0,20])
   .translate([width / 2, height / 2]);
 
 // Data and color scale
-var data = d3.map();
-var colorScale = d3.scaleThreshold()
+let data = new Map()
+const colorScale = d3.scaleThreshold()
   .domain([100000, 1000000, 10000000, 30000000, 100000000, 500000000])
   .range(d3.schemeBlues[7]);
 
 // Load external data and boot
-d3.queue()
-  .defer(d3.json, "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson")
-  .defer(d3.csv, "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world_population.csv", function(d) { data.set(d.code, +d.pop); })
-  .await(ready);
+Promise.all([
+d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson"),
+d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world_population.csv", function(d) {
+    data.set(d.code, +d.pop)
+})
+]).then(function(loadData){
+    let topo = loadData[0]
 
-function ready(error, topo) {
+    mouseOver = function(event,d) {
+      tip.style("opacity", 1)
+     .html("hello i am tip")//d.country + "<br/> Gold: " + d.gold + "<br/> Silver: " + d.silver)
+     .style("left", (event.x-25) + "px")
+     .style("top", (event.y-75) + "px");
 
-  let mouseOver = function(d) {
-    d3.selectAll(".Country")
-      .transition()
-      .duration(200)
-      .style("opacity", .5)
-    d3.select(this)
-      .transition()
-      .duration(200)
-      .style("opacity", 1)
-      .style("stroke", "black")
-  }
+      // d3.selectAll(".Country")
+      //   .transition()
+      //   .duration(200)
+      //   .style("opacity", .5);
+      // d3.select(this)
+      //   .transition()
+      //   // .duration(200)
+      //   .style("opacity", 1)
+      //   .style("stroke", "black");
+      
+    
+    }
+  
+     mouseLeave = function(d) {
 
-  let mouseLeave = function(d) {
-    d3.selectAll(".Country")
-      .transition()
-      .duration(200)
-      .style("opacity", .8)
-    d3.select(this)
-      .transition()
-      .duration(200)
-      .style("stroke", "transparent")
-  }
+      tip.style("opacity", 0);
+      // d3.selectAll(".Country")
+      //   .transition()
+      //   .duration(200)
+      //   .style("opacity", .8);
+      // d3.select(this)
+      //   .transition()
+      //   // .duration(200)
+      //   .style("stroke", "white");
+    }
 
-  // Draw the map
+
+
+
+    // Draw the map
   svg.append("g")
     .selectAll("path")
     .data(topo.features)
-    .enter()
-    .append("path")
+    .join("path")
       // draw each country
       .attr("d", d3.geoPath()
         .projection(projection)
@@ -61,10 +77,11 @@ function ready(error, topo) {
       .attr("fill", function (d) {
         d.total = data.get(d.id) || 0;
         return colorScale(d.total);
-      })
-      .style("stroke", "transparent")
-      .attr("class", function(d){ return "Country" } )
-      .style("opacity", .8)
+      })  
+      .attr('class','Country')
       .on("mouseover", mouseOver )
-      .on("mouseleave", mouseLeave )
-    }
+      .on("mouseleave", mouseLeave );
+
+      console.log(d3.selectAll(".Country"));
+      
+})
